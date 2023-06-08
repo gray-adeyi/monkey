44d427376b82bf19f258848878d194ca0b7683a3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-06-08 09:22:44 +0100

    Add token types

--- token/token.go
+++ token/token.go
@@ -1,5 +1,31 @@
 package token
 
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    // Identifiers + literals
+    IDENT = "IDENT" // add, foo, x, y,...
+    INT = "INT" // 12345
+
+    // Operators
+    ASSIGN = "="
+    PLUS = "+"
+
+    // Delimeters
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    // Keywords
+    FUNCTION
+    LET
+)
+
 type TokenType string
 
 type Token struct {
