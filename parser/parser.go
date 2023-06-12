c4c06271eb3a952afd1a15021421f8ad1aa0df37
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-06-12 14:18:03 +0100

    Add implementation for parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -55,6 +55,8 @@ func (p *Parser) parseStatement() ast.Statement{
     switch p.currToken.Type {
     case token.LET:
         return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
     default:
         return nil
     }
@@ -74,6 +76,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement{
     }
 
     // TODO: We're skipping the expressions until we
+    // encounter a semicolon
+    for !p.currTokenIs(token.SEMICOLON){
+        p.nextToken()
+    }
+    return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+    stmt := &ast.ReturnStatement{Token: p.currToken}
+
+    p.nextToken()
+
+    // TODO: We're skipping the expressions untill we
     // encounter a semicolon
     for !p.currTokenIs(token.SEMICOLON){
         p.nextToken()
