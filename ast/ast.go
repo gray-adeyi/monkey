ef38080b28124933dca7eddbbdeb354b773cb07e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-06-12 15:11:57 +0100

    Extend nodes in AST to provide String method

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+    "bytee"
     "github.com/gray-adeyi/monkey/token"
 )
 
 type Node interface {
     TokenLiteral() string
+    String() string
 }
 
 type Statement interface{
@@ -30,14 +32,37 @@ func (p *Program) TokenLiteral() string {
     }
 }
 
+func(p *Program) String() string{
+    var out bytes.Buffer
+    
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+    return out.String()
+}
+
 type LetStatement struct {
     Token token.Token  // The token token.LET
     Name *Identifier
     Value Expression
 }
 
-func (ls *LetStatement)statementNode() {}
-func (ls *LetStatement)TokenLiteral() string{ return ls.Token.Literal}
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string{ return ls.Token.Literal}
+
+func (ls *LetStatement) String() string{
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+    out.WriteString(";")
+    return out.String()
+}
 
 
 type Identifier struct{
@@ -49,6 +74,10 @@ func (i *Identifier) expressionNode(){}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+func (i *Identifier) String() string{
+    return i.Value
+}
+
 type ReturnStatement struct{
     Token token.Token // the 'return' token
     ReturnValue Expression
@@ -57,6 +86,17 @@ type ReturnStatement struct{
 func (rs *ReturnStatement) statementNode(){}
 func (rs *ReturnStatement) TokenLiteral() string{
     return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string{
+    var out bytes.Buffer
+    out.WriteString(rs.TokenLiteral() + " ")
+
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+    out.WriteString(";")
+    return out.String()
 }
 
 func ExpressionStatement{
@@ -69,4 +109,9 @@ func (es *ExpressionStatement) TokenLiteral() string{
     return es.Token.Literal
 }
 
-
+func (es *ExpressionStatement) String() string{
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
+}
